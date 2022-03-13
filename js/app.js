434ba7d2a059ba2f05d8c7fd4165bc92c62ae64e
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-03-13 20:18:13 +0300

    Add imports from script.js, add functionality to capture form input and update jQuery event methods used

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
+/**
+ * Imports
+ */
+import { processFormInputs } from './script.js';
+
 /** 
  * Toggle between description and icon when service icon is clicked in what we do section 
 */
 // Design service icon
-$("div#design-service").click(function(){
+$("div#design-service").on("click", function(){
   $("div#design-service-icon").toggle();
   $("div#design-service-description").toggle();
 });
 
 // Development service icon 
-$("div#dev-service").click(function(){
+$("div#dev-service").on("click",function(){
   $("div#dev-service-icon").toggle();
   $("div#dev-service-description").toggle();
 });
 
 // Product management icon
-$("div#product-mgmt-service").click(function(){
+$("div#product-mgmt-service").on("click", function(){
   $("div#product-mgmt-service-icon").toggle();
   $("div#product-mgmt-service-description").toggle();
 });
@@ -23,67 +28,83 @@ $("div#product-mgmt-service").click(function(){
  * Display white box with project name when portfolio project image is in hover state 
 */
 // Project 1
-$("div#project-dark-theme").hover(function(){
+$("div#project-dark-theme").on({
+  mouseenter: function(){
   $("div#project-name-1").toggle(300);
-},
-function(){
-  $("div#project-name-1").toggle(300);
+  },
+  mouseleave: function(){
+    $("div#project-name-1").toggle(300);
+  }
 });
 
 // Project 2
-$("div#project-crime-analytics").hover(function(){
+$("div#project-crime-analytics").on({
+  mouseenter: function(){
   $("div#project-name-2").toggle(300);
-},
-function(){
-  $("div#project-name-2").toggle(300);
+  },
+  mouseleave: function(){
+    $("div#project-name-2").toggle(300);
+  }
 });
 
 // Project 3
-$("div#project-orange-theme").hover(function(){
+$("div#project-orange-theme").on({
+  mouseenter: function(){
   $("div#project-name-3").toggle(300);
-},
-function(){
-  $("div#project-name-3").toggle(300);
+  },
+  mouseleave: function(){
+    $("div#project-name-3").toggle(300);
+  }
 });
 
 // Project 4
-$("div#project-movie-feed").hover(function(){
+$("div#project-movie-feed").on({
+  mouseenter: function(){
   $("div#project-name-4").toggle(300);
-},
-function(){
-  $("div#project-name-4").toggle(300);
+  },
+  mouseleave: function(){
+    $("div#project-name-4").toggle(300);
+  }
 });
 
 // Project 5
-$("div#project-resort-website").hover(function(){
+$("div#project-resort-website").on({
+  mouseenter: function(){
   $("div#project-name-5").toggle(300);
-},
-function(){
-  $("div#project-name-5").toggle(300);
+  },
+  mouseleave: function(){
+    $("div#project-name-5").toggle(300);
+  }
 });
 
 // Project 6
-$("div#project-button-design").hover(function(){
+$("div#project-button-design").on({
+  mouseenter: function(){
   $("div#project-name-6").toggle(300);
-},
-function(){
-  $("div#project-name-6").toggle(300);
+  },
+  mouseleave: function(){
+    $("div#project-name-6").toggle(300);
+  }
 });
 
 // Project 7
-$("div#project-logo-design").hover(function(){
+$("div#project-logo-design").on({
+  mouseenter: function(){
   $("div#project-name-7").toggle(300);
-},
-function(){
-  $("div#project-name-7").toggle(300);
+  },
+  mouseleave: function(){
+    $("div#project-name-7").toggle(300);
+  }
 });
 
 // Project 8
-$("div#project-restaurant-website").hover(function(){
+$("div#project-restaurant-website").on({
+  mouseenter: function(){
   $("div#project-name-8").toggle(300);
-},
-function(){
-  $("div#project-name-8").toggle(300);
+  },
+  mouseleave: function(){
+    $("div#project-name-8").toggle(300);
+  }
 });
 
 
@@ -93,3 +114,16 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 });
 
+//Handling input from contact us form
+$("form#contact-form").on("submit",function(event){
+  //Prevent form submission
+  event.preventDefault();
+
+  // Capture input
+  var name = $("input#name").val();
+  var email = $("input#email").val();
+  var message = $("textarea#message").val();
+  console.log(name, email, message);
+
+  processFormInputs(name, email, message);
+})
