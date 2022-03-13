8d364a70a0d243d181e3f86cfb0c358f70769cbc
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-03-13 15:35:03 +0300

    Add UI behaviour: display project name on project image's hover state in portfolio section

--- js/app.js
+++ js/app.js
@@ -19,4 +19,69 @@ $("div#product-mgmt-service").click(function(){
   $("div#product-mgmt-service-description").toggle();
 });
 
+/** 
+ * Display white box with project name when portfolio project image is in hover state 
+*/
+// Project 1
+$("div#project-dark-theme").hover(function(){
+  $("div#project-name-1").toggle(300);
+},
+function(){
+  $("div#project-name-1").toggle(300);
+});
 
+// Project 2
+$("div#project-crime-analytics").hover(function(){
+  $("div#project-name-2").toggle(300);
+},
+function(){
+  $("div#project-name-2").toggle(300);
+});
+
+// Project 3
+$("div#project-orange-theme").hover(function(){
+  $("div#project-name-3").toggle(300);
+},
+function(){
+  $("div#project-name-3").toggle(300);
+});
+
+// Project 4
+$("div#project-movie-feed").hover(function(){
+  $("div#project-name-4").toggle(300);
+},
+function(){
+  $("div#project-name-4").toggle(300);
+});
+
+// Project 5
+$("div#project-resort-website").hover(function(){
+  $("div#project-name-5").toggle(300);
+},
+function(){
+  $("div#project-name-5").toggle(300);
+});
+
+// Project 6
+$("div#project-button-design").hover(function(){
+  $("div#project-name-6").toggle(300);
+},
+function(){
+  $("div#project-name-6").toggle(300);
+});
+
+// Project 7
+$("div#project-logo-design").hover(function(){
+  $("div#project-name-7").toggle(300);
+},
+function(){
+  $("div#project-name-7").toggle(300);
+});
+
+// Project 8
+$("div#project-restaurant-website").hover(function(){
+  $("div#project-name-8").toggle(300);
+},
+function(){
+  $("div#project-name-8").toggle(300);
+});
